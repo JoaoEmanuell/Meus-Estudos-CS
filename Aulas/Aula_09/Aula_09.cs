6db48cb9e30510df0df48315e811668f67ccc50d
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-20 16:21:18 -0300

    Aulaa 09 full

---
+++ Aulas/Aula_09/Aula_09.cs
@@ -0,0 +1,9 @@
+﻿namespace Meus_Estudos_CS{
+    class Aula_09{
+        static void main(){
+            int number = 40;
+            number = number >> 2;
+            Console.WriteLine(number);
+        }
+    }
+}
