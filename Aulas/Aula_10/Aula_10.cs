2b146c1652092df98474243dcd4c79dd2509f972
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-20 16:45:57 -0300

    Aulaa 010 full

---
+++ Aulas/Aula_10/Aula_10.cs
@@ -0,0 +1,16 @@
+﻿namespace Meus_Estudos_CS{
+    class Aula_10{
+        enum WeekDays{ Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+        static void main(){
+            WeekDays wd = WeekDays.Monday;
+
+            //WeekDays wd = (WeekDays)3;
+
+            Console.WriteLine(wd);
+
+            int wd2 = (int)WeekDays.Monday;
+
+            Console.WriteLine(wd2);
+        }
+    }
+}
