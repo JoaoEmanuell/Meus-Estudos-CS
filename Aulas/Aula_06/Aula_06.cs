b2e2c3af1ed9fe2973c865682610c1572c43c596
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-20 10:48:43 -0300

    Aula 06 full

---
+++ Aulas/Aula_06/Aula_06.cs
@@ -0,0 +1,9 @@
+﻿class Aula_06{
+    static void main(){
+        double price, sale, gain;
+        price = 5.50; sale = 2.00; gain = 0.1;
+        Console.WriteLine($"Produto {price,15}");
+        Console.WriteLine($"Venda {sale,15:c}");
+        Console.WriteLine($"Lucro {gain,15:p}");
+    }
+}
