66224448bfa05680836bff44604095ec8473cf49
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-22 18:07:07 -0300

    Aula 17 full

---
+++ Aulas/Aula_17/Aula_17.cs
@@ -0,0 +1,18 @@
+﻿namespace Meus_Estudos_CS{
+    class Aula_17{
+        static void main(){
+            //int[] numbers = new int[5];
+            /*
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 16;
+            numbers[3] = 32;
+            numbers[4] = 64;
+            */
+            //int[] numbers = new int[3]{1,2,3};
+            int[] numbers = {1,2,3};
+            //Console.WriteLine($"The first number is {numbers[0]}");
+            Console.WriteLine($"The first number is {numbers[0]}");
+        }
+    }
+}
