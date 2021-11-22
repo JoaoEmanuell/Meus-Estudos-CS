164d942529034a325a9a973556f22632c15d490e
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-22 17:49:17 -0300

    Aula 16 full

---
+++ Aulas/Aula_16/Aula_16.cs
@@ -0,0 +1,34 @@
+﻿namespace Meus_Estudos_CS{
+    class Aula_16{
+        static void main(){
+            start : 
+
+            Console.Clear();
+            string name;
+            Console.Write("Type your name: ");
+            name = "0";//Console.ReadLine();
+
+            switch(name){
+                case "João":
+                    Console.WriteLine("Your name is João");
+                    break;
+                case "Maria":
+                    Console.WriteLine("Your name is Maria");
+                    break;
+                case "Pedro":
+                    Console.WriteLine("Your name is Pedro");
+                    break;
+                default:
+                    Console.WriteLine("Name not found");
+                    break;
+            }
+
+            Console.Write("Restart ? [s/n] ");
+            char answer = '0';//Convert.ToChar(Console.ReadLine());
+
+            if (answer == 's'){
+                goto start;
+            }
+        }
+    }
+}
