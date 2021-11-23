b0be0ef8c0b902460f2675810bc645293968ba48
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-23 17:20:16 -0300

    Aula 19 full

---
+++ Aulas/Aula_19/Aula_19.cs
@@ -0,0 +1,18 @@
+﻿namespace Meus_Estudos_CS{
+    class Aula_19{
+        static void main(){
+            /*
+            for (short i = 1; i <= 10; i++){
+                Console.WriteLine(i);
+            }
+            */
+            int[] numeros = new int[10];
+            for (short i = 0; i < numeros.Length; i++){
+                numeros[i] = 0;
+            }
+            for (short i = 0; i < numeros.Length; i++){
+                Console.WriteLine($"{i} : {numeros[i]}");
+            }
+        }
+    }
+}
