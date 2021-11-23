af7762479044213ec54b6036e8565370a6c3fc2f
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-23 17:04:52 -0300

    Aula 18 full

---
+++ Aulas/Aula_18/Aula_18.cs
@@ -0,0 +1,11 @@
+﻿namespace Meus_Estudos_CS{
+    class Aula_18{
+        static void main(){
+            int[,] matriz = new int[3,3];
+            matriz[0,0] = 1;
+            matriz[0,1] = 2;
+            matriz[0,2] = 3;
+            Console.WriteLine(matriz[0,2]);
+        }
+    }
+}
